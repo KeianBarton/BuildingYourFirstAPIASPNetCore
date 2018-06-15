8370b36452e94e72c03b3ab5d183a18668c98448
KeianBarton <Keian.Barton@civica.co.uk>
2018-06-15 09:56:52 +0100

    Creating project and first API

---
+++ CityInfo/CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,54 @@
+﻿using CityInfo.API.Models;
+using System.Collections.Generic;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+        public List<CityDto> Cities { get; set; }
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "New York City",
+                    Description = "The one with the big park.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 1,
+                            Name = "Central Park",
+                            Description = "The most visisted urban park in the US."
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 2,
+                            Name = "Empire State Building",
+                            Description = "A 102-story skyscraper located in Midtown Manhattan."
+                        }
+                    }
+                },
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Antwerp",
+                    Description = "The one with the catherdral that was never really finished.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 1,
+                            Name = "Foo",
+                            Description = "Foo bar."
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
